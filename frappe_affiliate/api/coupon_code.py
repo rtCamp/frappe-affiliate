966439a2a7c180432e944e006017261f67a470dc
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2025-09-03 17:03:15 +0530

    Update get_banners API to fetch file image

--- frappe_affiliate/api/coupon_code.py
+++ frappe_affiliate/api/coupon_code.py
@@ -3,13 +3,25 @@ import frappe
 @frappe.whitelist()
 def get_banners():
     banners = frappe.get_single("Affiliate Settings").banners
-    return banners
+    banners_list = []
+    for banner in banners:
+        banner_dict = {
+            "name": banner.name,
+            "image": frappe.utils.get_url(frappe.db.get_value("File", banner.banner_link, "file_url")),
+            "url": banner.url,
+            "title": banner.title,
+            "description": banner.description,
+            "width":banner.width,
+            "height":banner.height
+        }
+        banners_list.append(banner_dict)
+    return banners_list
 
 @frappe.whitelist()
 def get_affiliate_coupons(affiliate_id):
     coupon_codes = frappe.db.get_list(
         "Coupon Code",
-        {"custom_sales_partner": affiliate_id},
+        filters={"custom_sales_partner": affiliate_id},
         fields=["coupon_code"],
         pluck="coupon_code",
     )
