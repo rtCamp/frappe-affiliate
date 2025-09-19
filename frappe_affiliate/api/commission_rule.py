aa82cd5266fe5848da7546d044ccbf59f68401c6
Aryan Kaushik <aryankaushik2023@gmail.com>
2025-09-19 07:29:14 +0000

    update commission rules to support user group

--- frappe_affiliate/api/commission_rule.py
+++ frappe_affiliate/api/commission_rule.py
@@ -19,8 +19,10 @@ def get_commission_rules():
     commission_rules = []
     for commission_rule in commission_rules_list:
         rule_doc = frappe.get_doc("Affiliate Commission Rule", commission_rule.name)
-        apply_on_items_code = [child.item_code for child in rule_doc.apply_on]
-        apply_except_items_code = [child.item_code for child in rule_doc.apply_except]
+        apply_on_items_code = [child.item_code for child in rule_doc.apply_on_item_code]
+        apply_except_items_code = [
+            child.item_code for child in rule_doc.apply_except_item_code
+        ]
         apply_on_items = frappe.get_list(
             "Item", {"item_code": ["in", apply_on_items_code]}, pluck="item_name"
         )
@@ -34,9 +36,10 @@ def get_commission_rules():
                 "subsequent_commission": commission_rule.subsequent_commission,
                 "disabled": commission_rule.disabled,
                 "priority": commission_rule.priority,
-                "apply_on": apply_on_items,
-                "apply_except": apply_except_items,
+                "apply_on_item_code": apply_on_items,
+                "apply_except_item_code": apply_except_items,
                 "comment": commission_rule.comment,
+                "apply_on_group": commission_rule.apply_on_group,
             }
         )
     return commission_rules
--- frappe_affiliate/api/sales_invoice.py
+++ frappe_affiliate/api/sales_invoice.py
@@ -23,6 +23,8 @@ def get_invoice_count(doc):
 
 
 def apply_commission_rules(doc):
+    if not doc.sales_partner or doc.sales_partner == "":
+        return
     invoice_item_codes = set(item.item_code for item in doc.items)
     commission_rules = frappe.get_all(
         "Affiliate Commission Rule", filters={"disabled": 0}, fields=["name"]
@@ -32,21 +34,45 @@ def apply_commission_rules(doc):
 
     invoice_count = get_invoice_count(doc)
 
+    sales_partner = doc.sales_partner
+    sales_partner_user = frappe.db.get_value(
+        "Sales Partner", sales_partner, "custom_user"
+    )
+    affiliate_user_group = frappe.get_all(
+        "User Group Member", filters={"user": sales_partner_user}, pluck="parent"
+    )
+
     for rule in commission_rules:
         rule_doc = frappe.get_doc("Affiliate Commission Rule", rule.name)
-        apply_on_rule_item_codes = set(child.item_code for child in rule_doc.apply_on)
-        apply_except_rule_item_codes = set(
-            child.item_code for child in rule_doc.apply_except
+        apply_on_rule_item_codes = set(
+            child.item_code for child in rule_doc.apply_on_item_code
         )
-        if invoice_item_codes.issubset(
-            apply_on_rule_item_codes
-        ) and not invoice_item_codes.intersection(apply_except_rule_item_codes):
+        apply_except_rule_item_codes = set(
+            child.item_code for child in rule_doc.apply_except_item_code
+        )
+        apply_on_group = rule.apply_on_group
+        if (
+            (
+                len(rule_doc.apply_on_item_code) == 0
+                or invoice_item_codes.issubset(apply_on_rule_item_codes)
+            )
+            and not invoice_item_codes.intersection(apply_except_rule_item_codes)
+            and (
+                apply_on_group == None
+                or apply_on_group == ""
+                or apply_on_group in affiliate_user_group
+            )
+        ):
+            if commission_rule:
+                if rule_doc.priority < commission_rule.priority:
+                    commission_rule = rule_doc
+            else:
+                commission_rule = rule_doc
             commission_rule = rule_doc
-            break
 
     commission_percent = None
     if commission_rule:
-        if invoice_count > 1:
+        if invoice_count < 1:
             commission_percent = (
                 commission_rule.get("first_commission", None)
                 or frappe.get_single("Affiliate Settings").first_commission_rate
