48e958516539e2918c94a72d7123427cf5445f11
Aryan Kaushik <aryankaushik2023@gmail.com>
2025-10-07 11:09:46 +0000

    Add lead log record function

---
+++ frappe_affiliate/api/affiliate_lead_log.py
@@ -0,0 +1,19 @@
+import frappe
+
+
+def record_lead(data, user):
+    affiliate = data["affiliate"]
+    if not affiliate:
+        return
+    lead_log = frappe.new_doc("Affiliate Lead Log")
+
+    lead_log.sales_partner = affiliate
+    lead_log.time = frappe.utils.now_datetime()
+    lead_log.remote_address = data.get("remote_address", None)
+    if data.get("banner", None):
+        lead_log.banner_id = data["banner"]
+    lead_log.keyword = data.get("keyword", None)
+    lead_log.referrer = data.get("referrer", None)
+    lead_log.first_visited = data.get("first_visited", None)
+    lead_log.user = user
+    lead_log.save(ignore_permissions=True)
