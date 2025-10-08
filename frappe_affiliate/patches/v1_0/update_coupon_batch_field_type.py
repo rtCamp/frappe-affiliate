699692b469f5324679e48c4b839b6ea474d90356
zeel-codder <zeelprajapati321@gmail.com>
2025-10-08 20:58:35 +0530

    Handle the case when we do not have affiliates

--- frappe_affiliate/patches/v1_0/update_coupon_batch_field_type.py
+++ frappe_affiliate/patches/v1_0/update_coupon_batch_field_type.py
@@ -3,12 +3,16 @@ from frappe.model.meta import get_table_columns
 
 
 def execute():
-    columns_to_drop = ["code_length", "coupons_count"]
+    try:
+        columns_to_drop = ["code_length", "coupons_count"]
 
-    # Get current columns in the table
-    current_columns = get_table_columns("Coupon Batch")
+        # Get current columns in the table
+        current_columns = get_table_columns("Coupon Batch")
 
-    for column in columns_to_drop:
-        if column in current_columns:
-            frappe.db.sql(f"ALTER TABLE `tabCoupon Batch` DROP COLUMN `{column}`")
-    frappe.db.commit()
+        for column in columns_to_drop:
+            if column in current_columns:
+                frappe.db.sql(f"ALTER TABLE `tabCoupon Batch` DROP COLUMN `{column}`")
+        # frappe.db.commit()
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "update_coupon_batch_field_type")
+        print(f"Error occurred: {e}")
