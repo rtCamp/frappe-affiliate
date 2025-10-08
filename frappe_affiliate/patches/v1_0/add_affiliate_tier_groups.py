ec8cbd4f7fe6225de0d66a41369aa1dc0b3173fd
Aryan Kaushik <aryankaushik2023@gmail.com>
2025-10-08 08:31:56 +0000

    Modify tier group patch

--- frappe_affiliate/patches/v1_0/add_affiliate_tier_groups.py
+++ frappe_affiliate/patches/v1_0/add_affiliate_tier_groups.py
@@ -5,8 +5,8 @@ def execute():
     if not frappe.db.exists("User Group", "Affiliate Tier 1"):
         frappe.get_doc(
             {"doctype": "User Group", "__newname": "Affiliate Tier 1"}
-        ).insert()
+        ).insert(ignore_mandatory=True)
     if not frappe.db.exists("User Group", "Affiliate Tier 2"):
         frappe.get_doc(
             {"doctype": "User Group", "__newname": "Affiliate Tier 2"}
-        ).insert()
+        ).insert(ignore_mandatory=True)
