426c25a0d129376dd8f56131863f9a8f9c50c9f5
Aryan Kaushik <aryankaushik2023@gmail.com>
2025-10-13 08:23:50 +0000

    Set maximum value on referral rate

--- frappe_affiliate/frappe_affiliate/doctype/affiliate_referral_fee_rule/affiliate_referral_fee_rule.py
+++ frappe_affiliate/frappe_affiliate/doctype/affiliate_referral_fee_rule/affiliate_referral_fee_rule.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2025, rtCamp and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class AffiliateReferralFeeRule(Document):
-    pass
+    def validate(self):
+        if self.first_referral_rate > 100 or self.subsequent_referral_rate > 100:
+            frappe.throw(_("Referral rates must be between 0 and 100."))
+
+        if self.apply_on_group:
+            group_exists = frappe.db.exists("User Group", self.apply_on_group)
+            if not group_exists:
+                frappe.throw(
+                    _("User Group {0} does not exist").format(self.apply_on_group)
+                )
